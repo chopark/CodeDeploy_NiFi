28b9ff21d756d02c2db2cac13de5a44d19960df2
Ubuntu <ubuntu@ip-172-31-17-180.ec2.internal>
2020-05-27 02:38:14 +0000

    Update cpu and network limit

---
+++ count_flowfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## USAGE
+## ./wait_and_process_results.sh (sleep time) (target groups)
+#### e.g. (sleep time): 60s, 10m, 1h
+#### e.g. (target groups): 1, 2, 3, ..."
+
+# Directories
+HOME="/home/ubuntu"
+NIFI_HOME="$HOME/jarvis-nifi"
+NIFI_LOG="$NIFI_HOME/logs"
+NIFI_SCRIPT="$NIFI_HOME/scripts"
+NIFI_BIN="$NIFI_HOME/bin"
+NIFI_RESULTS="$NIFI_HOME/results"
+MINIFI_DIR="$HOME/minifi"
+MINIFI_HOME="$MINIFI_DIR/minifi-0.5.0"
+MINIFI_BIN="$MINIFI_HOME/bin"
+MINIFI_SCRIPT="$MINIFI_DIR/scripts"
+
+cat $NIFI_LOG/nifi-app.log | grep -c "o.a.n.r.p.s.SocketFlowFileServerProtocol SocketFlowFile"
