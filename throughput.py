fa87d55b43671b410c014c21b0681d1d4f930b3a
Ubuntu <ubuntu@ip-172-31-62-49.ec2.internal>
2020-07-17 02:29:29 +0000

    Fix throughput calcu

--- throughput.py
+++ throughput.py
@@ -1,7 +1,7 @@
 import sys
 
-def calcu(flow_num, time):
-   return flow_num * 0.419746399 / time
+def calcu(flow_num, time, edge_num):
+   return flow_num * 27.2 / time / edge_num
 
 if __name__ == "__main__":
-    print("Throughput: ",calcu(float(sys.argv[1]), float(sys.argv[2])),"MBps")
+    print("Throughput: ",calcu(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])),"Mbps")
