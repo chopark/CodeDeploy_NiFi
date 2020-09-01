4444493fa520c9a15df035bc5926cd35f31df88c
Ubuntu <ubuntu@ip-172-31-17-44.ec2.internal>
2020-09-01 10:47:53 +0000

    Update dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 cp .bash_aliases ../
 sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install iperf -y
 sudo dpkg -i jdk/*
-sudo apt-get install
 
-sudo apt install awscli -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 aws configure
 source ~/.bashrc
